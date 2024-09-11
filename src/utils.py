921915646395789cb59ae40ce64a15c97079f19b
Clemens W <clemenswalte@web.de>
2024-09-11 08:08:10 +0200

    Addition of investment horizon

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,12 @@
+import datetime
 import polars as pl
 
-from config import DATE_SEP
+from config import DATE_SEP, DATE_FORMAT
 
-def find_next_date_in_df(df:pl.DataFrame, date:str)->str:
-    date_tuple = (int(x) for x in date.split(DATE_SEP))
-    relevant_dates = df.filter(pl.col('Date') >= pl.date(*date_tuple))
+def find_next_date_in_df(df:pl.DataFrame, date:str, investment_horizon=0)->str:
+    pl_date = pl.date(*(int(x) for x in date.split(DATE_SEP)))
+    relevant_dates = df.filter(pl.col('Date') >= pl_date)
     next_date_df = relevant_dates.select(pl.col('Date').min())
-    
     # Check if data from the same or a later date available, else return empty string
     if not next_date_df.is_empty():
         next_date = next_date_df['Date'].dt.strftime('%Y-%m-%d')[0]
@@ -20,3 +20,8 @@ def get_polars_date_from_str(date_str:str)->pl.Date:
 
 def get_price_of_day(df:pl.DataFrame, date_str:str, price_type='Close')->float:
     return df.filter(pl.col('Date')==get_polars_date_from_str(date_str))[price_type][0]
+
+def add_n_years_to_date(date:str, n_years:int)->str:
+    shifted_date = datetime.datetime.strptime(date, DATE_FORMAT) + datetime.timedelta(days=n_years*365)
+    shifted_date_str = shifted_date.strftime(DATE_FORMAT)
+    return shifted_date_str
